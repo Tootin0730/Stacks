f524f1e5e382ea88c647b4b6a09ae38927c61729
Tootin0730 <yisan1004@gmail.com>
2023-06-07 10:17:30 +0900

    Stacks Implementation (Array)

---
+++ src/academy/learnprograming/Stacks/ArrayStack.java
@@ -0,0 +1,41 @@
+package academy.learnprograming.Stacks;
+
+import java.util.EmptyStackException;
+
+public class ArrayStack {
+    private Employee[] stack;
+    private int top;
+
+    public ArrayStack(int capacity) {
+        stack = new Employee[capacity];
+    }
+
+    public void push(Employee employee) {
+        if (top == stack.length) {
+            // need to resize the backing array
+            Employee[] newArray = new Employee[2 * stack.length];
+            System.arraycopy(stack, 0, newArray, 0, stack.length);
+            stack = newArray;
+        }
+        stack[top++] = employee;
+    }
+
+    public Employee pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        Employee employee = stack[--top];
+        stack[top] = null;
+        return employee;
+    }
+
+    public Employee peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack[top - 1];
+    }
+    public boolean isEmpty() {
+        return top == 0;
+    }
+}
---
+++ src/academy/learnprograming/Stacks/Employee.java
@@ -0,0 +1,42 @@
+package academy.learnprograming.Stacks;
+
+public class Employee {
+
+    private final String firstName;
+    private final String lastName;
+    private final int id;
+
+    public Employee(String firstName, String lastName, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (id != employee.id) return false;
+        if (!firstName.equals(employee.firstName)) return false;
+        return lastName.equals(employee.lastName);
+    }
+
+    public int hashCode() {
+        int result = firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + id;
+        return result;
+    }
+
+    public String toString() {
+        return "Employee{ " +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+}
+
